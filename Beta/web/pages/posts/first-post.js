85748085ae7b427caac875976f6773697a76b4df
graweedman <43838489+graweedman@users.noreply.github.com>
2022-02-17 22:31:55 +0200

    new framework

---
+++ Beta/web/pages/posts/first-post.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import image from 'next/image'
+import Script from 'next/script'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </Layout>
+    )
+  }
