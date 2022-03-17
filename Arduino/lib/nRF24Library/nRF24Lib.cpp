562741ed3e9a4d2f7df47dbf538eb984774519a3
Kunkerlv <balodis10@gmail.com>
2022-03-17 20:43:53 +0200

    Library update

--- Arduino/lib/nRF24Library/nRF24Lib.cpp
+++ Arduino/lib/nRF24Library/nRF24Lib.cpp
@@ -1,47 +1,166 @@
 #include "nRF24Lib.h"
 
-void nRF24Lib::nRF24_RegisterWrite(uint8_t reg, uint8_t regbit, uint8_t val){
-    digitalWrite(CSNpin, HIGH);
-    SPI.transfer((W_REGISTER  || reg));
-    if(val){
-        SPI.transfer(1 << regbit);
-    }
-    else{
-        SPI.transfer(0 << regbit);
-    }
+void nRF24Lib::CS_Select(void){
     digitalWrite(CSNpin, LOW);
 }
 
-//Inicialize kontrolieri un iestata RX rezima
-void nRF24Lib::nRF24_Init(){
-    _delay_ms(110);
-    nRF24_RegisterWrite(CONFIG, PWR_UP, 1);
-    _delay_ms(2);
+void nRF24Lib::CS_UnSelect(void){
+    digitalWrite(CSNpin, HIGH);
+}
+
+void nRF24Lib::CE_Enable(void){
     digitalWrite(CSpin, HIGH);
-    nRF24_RegisterWrite(CONFIG, BITPOS_PRIM_RX, 1);
 }
 
-void nRF24Lib::nRF24_WriteTXData(uint8_t* pTXdata, uint8_t asize){
-         digitalWrite(CSNpin, HIGH);
-    SPI.transfer(W_TX_PAYLOAD);
-    for(int i = 0; i <= asize; i++){
-        SPI.transfer(pTXdata[i]);
+void nRF24Lib::CE_Disable(void){
+    digitalWrite(CSpin, LOW);
+}
+
+void nRF24Lib::WriteRegister(uint8_t reg, uint8_t data){
+    uint8_t buf[2];
+    buf[1] = reg|1<<5;
+    buf[2] = data;
+
+    CS_Select();
+    SPI.transfer(buf, 2);
+    CS_UnSelect();
+
+}
+
+void nRF24Lib::WriteRegisterMulti(uint8_t reg, uint8_t * data, int size){
+    uint8_t buf[2];
+    buf[1] = reg|1<<5;
+
+    CS_Select();
+    SPI.transfer(buf, 1);
+    SPI.transfer(data, size);
+    CS_UnSelect();
+
+}
+
+uint8_t nRF24Lib::ReadRegister(uint8_t reg){
+    uint8_t dati = 0;
+
+    CS_Select();
+    dati = SPI.transfer(reg);
+    CS_UnSelect();
+
+    return dati;
+}
+
+void nRF24Lib::ReadRegisterMulti(uint8_t reg, uint8_t *data, int size){
+	CS_Select();
+
+	SPI.transfer(&reg, 1);
+	SPI.transfer(data, size);
+
+	CS_UnSelect();
+}
+
+void nRF24Lib::SendCommand(uint8_t cmd){
+    CS_Select();
+    SPI.transfer(&cmd, 1);
+    CS_UnSelect();
+}
+
+void nRF24Lib::Init(void){
+    CE_Disable();
+
+    WriteRegister(CONFIG, 0);
+
+    WriteRegister(EN_AA, 0);
+
+    WriteRegister(EN_RXADDR, 0);
+
+    WriteRegister(SETUP_AW, 0x03);
+
+    WriteRegister(SETUP_RETR, 0);
+
+    WriteRegister(RF_CH, 0);
+
+    WriteRegister(RF_SETUP, 0x0E);
+
+    CE_Enable();
+}
+
+void nRF24Lib::TX_Mode(uint8_t *address, uint8_t ch){
+    CE_Disable();
+
+    WriteRegister(RF_CH, ch);
+    WriteRegisterMulti(TX_ADDR, address, 5);
+
+    uint8_t cfg = ReadRegister(CONFIG);
+    cfg = cfg | (1<<1);
+    WriteRegister(CONFIG, cfg);
+
+    CE_Enable();
+}
+
+uint8_t nRF24Lib::Transmit(uint8_t *dati){
+    uint8_t cmdsend = 0;
+
+    CS_Select();
+    cmdsend = W_TX_PAYLOAD;
+    SPI.transfer(&cmdsend, 1);
+    SPI.transfer(dati, 32);
+    CS_UnSelect();
+
+    delay(1);
+
+    uint8_t fifostatus;
+    fifostatus = ReadRegister(FIFO_STATUS);
+
+    if ((fifostatus&(1<<4)) && (!(fifostatus&(1<<3)))){
+            cmdsend = FLUSH_TX;
+            SendCommand(cmdsend);
+
+            return 1;
     }
-    digitalWrite(CSNpin, LOW);
+
+return 0;
 }
 
-//Iestata TX rezima
-void nRF24Lib::nRF24_ENTXMode(){
-    nRF24_RegisterWrite(CONFIG, BITPOS_PRIM_RX, 0);
+void nRF24Lib::RX_Mode(uint8_t *address, uint8_t ch){
+    CE_Disable();
+
+    WriteRegister(RF_CH, ch);
+
+    uint8_t enrxaddr = ReadRegister(EN_RXADDR);
+    enrxaddr = enrxaddr | (1<<1);
+    WriteRegister(EN_RXADDR, enrxaddr);
+
+    WriteRegisterMulti(RX_ADDR_P1, address, 5);
+
+    WriteRegister(RX_PW_P1, 32);
+
+    uint8_t cfg = ReadRegister(CONFIG);
+    cfg = cfg | (1<<1) | (1<<0);
+    WriteRegister(CONFIG, cfg);
+
+    CE_Enable();
 }
 
-void nRF24Lib::FlushTX(){
-    SPI.transfer(FLUSH_TX);
+uint8_t nRF24Lib::IsDataAvailable(uint8_t pipenr){
+    uint8_t status = ReadRegister(STATUS);
+    
+    if((status&(1<<6))&&(status&(pipenr<<1))){
+        WriteRegister(STATUS, (1<<6));
+        return 1;
+    }
+    return 0;
 }
 
-uint8_t nRF24Lib::nRF24_ReadRXBuffer(){
-    digitalWrite(CSNpin, HIGH);
-    * RXbuffer = SPI.transfer(R_RX_PAYLOAD);
-    digitalWrite(CSNpin, LOW);
-    return *RXbuffer;
+void nRF24Lib::Receive(uint8_t *data){
+    uint8_t cmdsend = 0;
+
+    CS_Select();
+    cmdsend = R_RX_PAYLOAD;
+    *data = SPI.transfer(cmdsend);
+    CS_UnSelect();
+
+    delay(1);
+
+    cmdsend = FLUSH_RX;
+    SendCommand(cmdsend);
+
 }
--- Arduino/lib/nRF24Library/nRF24Lib.h
+++ Arduino/lib/nRF24Library/nRF24Lib.h
@@ -1,188 +1,74 @@
-#pragma once
-
-//Standarta bibliotekas 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-//Arduino bibliotekas
 #include <Arduino.h>
 #include <SPI.h>
 
-//Biti
-#define	BIT0	(1 << 0)
-#define	BIT1	(1 << 1)
-#define	BIT2	(1 << 2)
-#define	BIT3	(1 << 3)
-#define	BIT4	(1 << 4)
-#define	BIT5	(1 << 5)
-#define	BIT6	(1 << 6)
-#define	BIT7	(1 << 7)
-
-//Registru adreses
-#define CONFIG 0x00
-#define EN_AA 0x01
-#define EN_RXADDR 0x02
-#define SETUP_AR 0x03
-#define SETUP_RETR 0x04
-#define RF_CH 0x05
-#define RF_SETUP 0x06
-#define STATUS 0x07
-#define OBSERVE_TX 0x08
-#define RPD 0x09
-#define RX_ADDR_P0 0x0A
-#define RX_ADDR_P1 0x0B
-#define RX_ADDR_P2 0x0C
-#define RX_ADDR_P3 0x0D
-#define RX_ADDR_P4 0x0E
-#define RX_ADDR_P5 0x0F
-#define TX_ADDR 0x10
-#define RX_PW_P0 0x11
-#define RX_PW_P1 0x12
-#define RX_PW_P2 0x13
-#define RX_PW_P3 0x14
-#define RX_PW_P4 0x15
-#define RX_PW_P5 0x16
+/* Registri*/
+#define CONFIG      0x00
+#define EN_AA       0x01
+#define EN_RXADDR   0x02
+#define SETUP_AW    0x03
+#define SETUP_RETR  0x04
+#define RF_CH       0x05
+#define RF_SETUP    0x06
+#define STATUS      0x07
+#define OBSERVE_TX  0x08
+#define CD          0x09
+#define RX_ADDR_P0  0x0A
+#define RX_ADDR_P1  0x0B
+#define RX_ADDR_P2  0x0C
+#define RX_ADDR_P3  0x0D
+#define RX_ADDR_P4  0x0E
+#define RX_ADDR_P5  0x0F
+#define TX_ADDR     0x10
+#define RX_PW_P0    0x11
+#define RX_PW_P1    0x12
+#define RX_PW_P2    0x13
+#define RX_PW_P3    0x14
+#define RX_PW_P4    0x15
+#define RX_PW_P5    0x16
 #define FIFO_STATUS 0x17
-#define DYNPD 0x1C
-#define FEATURE 0x1D
+#define DYNPD	    0x1C
+#define FEATURE	    0x1D
 
-// Registru biti
-#define MASK_RX_DR  BIT6
-#define MASK_TX_DS  BIT5
-#define MASK_MAX_RT BIT4
-#define EN_CRC      BIT3
-#define CRCO        BIT2
-#define PWR_UP      BIT1
-#define PRIM_RX     BIT0
-#define ENAA_P5     BIT5
-#define ENAA_P4     BIT4
-#define ENAA_P3     BIT3
-#define ENAA_P2     BIT2
-#define ENAA_P1     BIT1
-#define ENAA_P0     BIT0
-#define ERX_P5      BIT5
-#define ERX_P4      BIT4
-#define ERX_P3      BIT3
-#define ERX_P2      BIT2
-#define ERX_P1      BIT1
-#define ERX_P0      BIT0
-#define DYNPL_P5	 BIT5
-#define DYNPL_P4	 BIT4
-#define DYNPL_P3	 BIT3
-#define DYNPL_P2	 BIT2
-#define DYNPL_P1	 BIT1
-#define DYNPL_P0	 BIT0
-#define AW          BIT0
-#define ARD         BIT4
-#define ARC         BIT0
-#define PLL_LOCK    BIT4
-#define CONT_WAVE   BIT7
-#define RF_DR       BIT3
-#define RF_DR_LOW   BIT5
-#define RF_DR_HIGH  BIT3
-#define RF_PWR      BIT1
-#define LNA_HCURR   BIT0
-#define RX_DR       BIT6
-#define TX_DS       BIT5
-#define MAX_RT      BIT4
-#define RX_P_NO     BIT1
-#define TX_FULL     BIT0
-#define PLOS_CNT    BIT4
-#define ARC_CNT     BIT0
-#define TX_REUSE    BIT6
-#define FIFO_FULL   BIT5
-#define TX_EMPTY    BIT4
-#define RX_FULL     BIT1
-#define RX_EMPTY    BIT0
-#define EN_DPL      BIT2
-#define EN_ACK_PAY  BIT1
-#define EN_DYN_ACK  BIT0
-
-// Bitu pozicijas
-#define BITPOS_MASK_RX_DR  6
-#define BITPOS_MASK_TX_DS  5
-#define BITPOS_MASK_MAX_RT 4
-#define BITPOS_EN_CRC      3
-#define BITPOS_CRCO        2
-#define BITPOS_PWR_UP      1
-#define BITPOS_PRIM_RX     0
-#define BITPOS_ENAA_P5     5
-#define BITPOS_ENAA_P4     4
-#define BITPOS_ENAA_P3     3
-#define BITPOS_ENAA_P2     2
-#define BITPOS_ENAA_P1     1
-#define BITPOS_ENAA_P0     0
-#define BITPOS_ERX_P5      5
-#define BITPOS_ERX_P4      4
-#define BITPOS_ERX_P3      3
-#define BITPOS_ERX_P2      2
-#define BITPOS_ERX_P1      1
-#define BITPOS_ERX_P0      0
-#define BITPOS_DYNPL_P5	 5
-#define BITPOS_DYNPL_P4	 4
-#define BITPOS_DYNPL_P3	 3
-#define BITPOS_DYNPL_P2	 2
-#define BITPOS_DYNPL_P1	 1
-#define BITPOS_DYNPL_P0	 0
-#define BITPOS_AW          0
-#define BITPOS_ARD         4
-#define BITPOS_ARC         0
-#define BITPOS_PLL_LOCK    4
-#define BITPOS_CONT_WAVE   7
-#define BITPOS_RF_DR       3
-#define BITPOS_RF_DR_LOW   5
-#define BITPOS_RF_DR_HIGH  3
-#define BITPOS_RF_PWR      1
-#define BITPOS_LNA_HCURR   0
-#define BITPOS_RX_DR       6
-#define BITPOS_TX_DS       5
-#define BITPOS_MAX_RT      4
-#define BITPOS_RX_P_NO     1
-#define BITPOS_TX_FULL     0
-#define BITPOS_PLOS_CNT    4
-#define BITPOS_ARC_CNT     0
-#define BITPOS_TX_REUSE    6
-#define BITPOS_FIFO_FULL   5
-#define BITPOS_TX_EMPTY    4
-#define BITPOS_RX_FULL     1
-#define BITPOS_RX_EMPTY    0
-#define BITPOS_EN_DPL      2
-#define BITPOS_EN_ACK_PAY  1
-#define BITPOS_EN_DYN_ACK  0
-
-//Instrukcijas
+/* Komandas */
 #define R_REGISTER    0x00
 #define W_REGISTER    0x20
 #define REGISTER_MASK 0x1F
+#define ACTIVATE      0x50
+#define R_RX_PL_WID   0x60
 #define R_RX_PAYLOAD  0x61
 #define W_TX_PAYLOAD  0xA0
+#define W_ACK_PAYLOAD 0xA8
 #define FLUSH_TX      0xE1
 #define FLUSH_RX      0xE2
 #define REUSE_TX_PL   0xE3
-#define R_RX_PL_WID   0x60
-#define W_ACK_PAYLOAD 0xA8
-#define W_TX_PAYLOAD_NOACK 0xB0
-#define ACTIVATE		0x50
 #define NOP           0xFF
 
 class nRF24Lib{
 
     public:
         
-        uint8_t CSNpin;
-        uint8_t CSpin;
-        uint8_t RXbuffer[31];
+    uint8_t CSNpin;
+    uint8_t CSpin;
 
-        nRF24Lib(uint8_t CSN, uint8_t CS){
+    nRF24Lib(uint8_t CSN, uint8_t CS){
             CSNpin = CSN;
             CSpin = CS;
         };
 
-        void nRF24_RegisterWrite(uint8_t reg, uint8_t regbit, uint8_t val);
-        void nRF24_Init();
-        void nRF24_WriteTXData(uint8_t* pTXdata, uint8_t asize);
-        void nRF24_ENTXMode();
-        void FlushTX();
-        uint8_t nRF24_ReadRXBuffer();
+
+    void CS_Select(void);
+    void CS_UnSelect(void);
+    void CE_Enable(void);
+    void CE_Disable(void);
+    void WriteRegister(uint8_t reg, uint8_t data);
+    void WriteRegisterMulti(uint8_t reg, uint8_t * data, int size);
+    uint8_t ReadRegister(uint8_t reg);
+    void ReadRegisterMulti(uint8_t reg, uint8_t * data, int size);
+    void SendCommand(uint8_t cmd);
+    void Init(void);
+    void TX_Mode(uint8_t *address, uint8_t ch);
+    uint8_t Transmit(uint8_t *dati);
+    void RX_Mode(uint8_t *address, uint8_t ch);
+    uint8_t IsDataAvailable(uint8_t pipenr);
+    void Receive(uint8_t *data);
 };
