4cd60845f909ed4ba6b8e4f634291fef7cda5203
graweedman <43838489+graweedman@users.noreply.github.com>
2022-05-20 12:53:51 +0300

    error

--- Web/components/tables/table.js
+++ Web/components/tables/table.js
@@ -4,8 +4,8 @@ export default function Table({ children , styles, content}) {
     return (
         <table className={styles.table}>
             {children}
-            {content.map(({date, temperature, humidity, brightness}) => (
-                <tr key={id} className={styles.tableRow}>
+            {content.map(({_id, date, temperature, humidity, brightness}) => (
+                <tr key={_id} className={styles.tableRow}>
                     <td>{new Date(date).toUTCString("dd/mm/yy")}</td>
                     {/* <td>{new Date(date).toTimeString()}</td> */}
                     <td>{temperature}</td>
