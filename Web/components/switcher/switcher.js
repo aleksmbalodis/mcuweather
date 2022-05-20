9f02abe186cc4b4b5810cb36a68f0aba276383a8
graweedman <43838489+graweedman@users.noreply.github.com>
2022-05-20 11:20:28 +0300

    buuild changes

--- Web/components/switcher/switcher.js
+++ Web/components/switcher/switcher.js
@@ -1,28 +1,28 @@
-import styles from "../../styles/Switcher.module.css"
-import LineGraph from "./LineGraph"
-import React, { useState, useEffect } from 'react'
-import Filter from "../filter"
+// import styles from "../../styles/Switcher.module.css"
+// import LineGraph from "./LineGraph"
+// import React, { useState, useEffect } from 'react'
+// import Filter from "../filter"
 
-export default function Switcher({graphs, children, data}) {
+// export default function Switcher({graphs, children, data}) {
 
     
-    // const [data, setData] = useState()
-    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+//     // const [data, setData] = useState()
+//     const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
 
-    const data = {
-        labels,
-        datasets: [
-            {
-                data: [0.1, 0.4, 0.2, 0.3, 0.7, 0.4, 0.6, 0.3],
-                borderColor: 'rgb(255,0,0)',
-                label: "Sex",
-            }
-        ],
-    };
+//     const data = {
+//         labels,
+//         datasets: [
+//             {
+//                 data: [0.1, 0.4, 0.2, 0.3, 0.7, 0.4, 0.6, 0.3],
+//                 borderColor: 'rgb(255,0,0)',
+//                 label: "Sex",
+//             }
+//         ],
+//     };
 
-    return (
-        <div className={styles.page}>
+//     return (
+//         <div className={styles.page}>
             
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
