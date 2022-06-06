61ca7d400cbdf1fc0a826b7d9b60f61ab8716b67
graweedman <43838489+graweedman@users.noreply.github.com>
2022-06-06 09:49:33 +0300

    Dokumentacija

--- Web/components/tables/table.js
+++ Web/components/tables/table.js
@@ -8,9 +8,9 @@ export default function Table({ children , styles, content}) {
                 <tr key={_id} className={styles.tableRow}>
                     <td>{new Date(date).toUTCString("dd/mm/yy")}</td>
                     {/* <td>{new Date(date).toTimeString()}</td> */}
-                    <td>{temperature}</td>
-                    <td>{humidity}</td>
-                    <td>{brightness}</td>
+                    <td>{temperature} C</td>
+                    <td>{humidity} %</td>
+                    <td>{brightness} lux</td>
                 </tr>
             ))}
         </table>
