b23ff7aeeef2a8f67d001dd1036695e673af0269
graweedman <niba12ww@gmail.com>
2022-06-17 14:32:11 +0300

    Final version

--- Web/db/dataManager.js
+++ Web/db/dataManager.js
@@ -2,23 +2,25 @@ import mongoose from 'mongoose';
 import conn from './connection';
 const { Schema } = mongoose;
 
+//schema created for storing data
 export const dataSchema = new Schema({
     date: Date,
-    // time: Mixed,
     temperature: Number,
     humidity: Number,
     brightness: Number
 })
 
-
+//creates a connection module which saves data in data table
 const model = conn().model("data", dataSchema, "data")
 
-
+//creates a new row in database
 export const add = async ({date, temperature, humidity, brightness}) => {
 
+    //Checks if data collumns are valid 
     if(!date || !temperature || !humidity || !brightness) return {status: 400, message: "Mising parameters"}
 
     try {
+        //Creates a row for database before uploading
         await model.create({
             date,
             temperature,
@@ -27,6 +29,7 @@ export const add = async ({date, temperature, humidity, brightness}) => {
         })
         return {status: 200, message:"Data Uploaded"}
     }
+    //catches error and returns it to client
     catch (err) {
         console.log(err)
         return {status: 500, message: "Unknown error"}
@@ -34,35 +37,40 @@ export const add = async ({date, temperature, humidity, brightness}) => {
     
 }
 
+//returns filtered or not filtered array of data from database
 export const list = async (filter) =>
 {
     var data
-
+    //checks if filter object is populated
     if (filter) {
+        //queries database for the filter data
         data = await model.find({ date: {
             $gte: filter.from,
             $lt: filter.to
         }})
     } 
     else {
+        //returns whole table without filter
         data = await model.find({})
     }
 
     
-    // console.log(model.find({}))
-    // console.log(data)
+    //returns final data
     return JSON.stringify(data)
     
 }
 
+//removes selected row
 export const remove = async (_id) => {
     try 
     {
+        //Deletes chosen row
         await model.deleteOne({_id})
         return {message: "Record deleted"}
     }
     catch (error)
     {
+        //upon error message is returned
         console.log(error)
         return {message: "Deletion failed"}
     }
